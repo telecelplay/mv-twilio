a38de69230cbb305a145b19e71903bf12419c912
Tony Alejandro <tonysviews@gmail.com>
2022-03-03 09:27:45 +0800

    update

--- facets/java/org/meveo/twilio/SendOtp.java
+++ facets/java/org/meveo/twilio/SendOtp.java
@@ -6,59 +6,40 @@ import java.util.HashMap;
 import java.util.Random;
 import java.time.Instant;
 import java.time.Duration;
-import javax.ws.rs.client.*;
-import javax.ws.rs.core.*;
-
-import org.meveo.admin.exception.BusinessException;
-import org.meveo.api.persistence.CrossStorageApi;
-import org.meveo.commons.utils.ParamBean;
-import org.meveo.commons.utils.ParamBeanFactory;
-import org.meveo.credentials.CredentialHelperService;
-import org.meveo.model.customEntities.Credential;
-import org.meveo.model.customEntities.OutboundSMS;
-import org.meveo.model.persistence.CEIUtils;
-import org.meveo.model.storage.Repository;
 import org.meveo.service.script.Script;
-import org.meveo.service.storage.RepositoryService;
-
+import org.meveo.model.customEntities.OutboundSMS;
+import org.meveo.admin.exception.BusinessException;
 import org.slf4j.LoggerFactory;
+import org.json.*;
+import javax.ws.rs.client.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.gson.*;
-
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
+import javax.ws.rs.core.*;
+import org.meveo.model.customEntities.Credential;
+import org.meveo.service.storage.RepositoryService;
+import org.meveo.model.storage.Repository;
+import org.meveo.api.persistence.CrossStorageApi;
+import org.meveo.model.persistence.CEIUtils;
+import org.meveo.credentials.CredentialHelperService;
 
 public class SendOtp extends Script {
-    private static final Logger LOG = LoggerFactory.getLogger(SendOtp.class);
-    private static final String TWILIO_URL = "api.twilio.com";
-
-    private CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
-    private RepositoryService repositoryService = getCDIBean(RepositoryService.class);
-    private Repository defaultRepo = repositoryService.findDefaultRepository();
-    private ParamBeanFactory paramBeanFactory = getCDIBean(ParamBeanFactory.class);
-    private ParamBean config = paramBeanFactory.getInstance();
-
-    private String retryDelay = config.getProperty("otp.retry.delay", "30");
-    private String retryLimit = config.getProperty("otp.retry.limit", "5");
-    private String otpAppName = config.getProperty("otp.app.name", "Telecel");
-    private String otpMessage = config
-            .getProperty("otp.message.format", "Your %s verification code is: %s");
-    private Duration retryTimeLimit = Duration.ofSeconds(Long.parseLong(retryDelay));
-    private int retryLimitPerDay = Integer.parseInt(retryLimit, 10);
 
     private String to;
+
     private String result;
 
     public String getResult() {
         return result;
     }
 
-    public void setTo(String to) {
-        this.to = to;
-    }
+    private static final Logger log = LoggerFactory.getLogger(SendOtp.class);
+    private static final String TWILIO_URL = "api.twilio.com";
+    private static final Duration MIN_DURATION = Duration.ofSeconds(30);
+    private static final int MAX_MESSAGE_IN_DAY = 5;
+
+    private CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
+    private RepositoryService repositoryService = getCDIBean(RepositoryService.class);
+    private Repository defaultRepo = repositoryService.findDefaultRepository();
 
     @Override
     public void execute(Map<String, Object> parameters) throws BusinessException {
@@ -66,17 +47,19 @@ public class SendOtp extends Script {
                 .by("to", to)
                 .by("purpose", "OTP")
                 .by("fromRange creationDate", Instant.now())
-                .orderBy("creationDate", false) // newest to oldest
+                .orderBy("creationDate", false) // order by descending creation date to retriev the
+                                                // newest one
                 .getResults();
 
         if (lastSMSList != null && lastSMSList.size() > 0) {
             OutboundSMS lastSMS = lastSMSList.get(0);
-            Duration lastRetry = Duration.between(lastSMS.getCreationDate(), Instant.now());
-            if (lastRetry.compareTo(retryTimeLimit) < 0) {
+            Duration duration = Duration.between(lastSMS.getCreationDate(), Instant.now());
+            if (duration.compareTo(MIN_DURATION) < 0) {
+                // throw new BusinessException("There is an ogoing Otp, please wait.");
                 result = "retry_later";
                 return;
             }
-            if (lastSMSList.size() > retryLimitPerDay) {
+            if (lastSMSList.size() >= MAX_MESSAGE_IN_DAY) {
                 result = "too_many_requests";
                 return;
             }
@@ -84,51 +67,58 @@ public class SendOtp extends Script {
         Credential credential =
                 CredentialHelperService.getCredential(TWILIO_URL, crossStorageApi, defaultRepo);
         if (credential == null) {
-            LOG.error("No credential found for " + TWILIO_URL);
+            // throw new BusinessException("No credential found for "+TWILIO_URL);
+            log.error("No credential found for " + TWILIO_URL);
             result = "server_error";
             return;
         } else {
-            LOG.info("using credential {} with username {}", credential.getUuid(),
+            log.info("using credential {} with username {}", credential.getUuid(),
                     credential.getUsername());
         }
         String TWILIO_SID = credential.getUsername();
-        String TWILIO_TOKEN = credential.getToken();
-        String TWILIO_PHONE_NUMBER = credential.getRefreshToken();
-
+        String TWILIO_MESSAGE_ID = credential.getToken();
+        String from = credential.getRefreshToken();
+        String url = "https://api.twilio.com/2010-04-01/Accounts/" + TWILIO_SID + "/Messages.json";
         Random rnd = new Random();
         String otp = String.format("%06d", rnd.nextInt(999999));
-        String body = String.format(otpMessage, otpAppName, otp);
-
-        Twilio.init(TWILIO_SID, TWILIO_TOKEN);
-        Message message = Message
-                .creator(
-                        new PhoneNumber(to),
-                        new PhoneNumber(TWILIO_PHONE_NUMBER),
-                        body)
-                .create();
-
-        String error = message.getErrorMessage();
-        if (error != null) {
-            LOG.error("Twilio error: {}", error);
+        String message = "Your telecelplay verification code is:" + otp;
+        Form map = new Form().param("To", to).param("MessagingServiceSid", TWILIO_MESSAGE_ID)
+                .param("Body", message).param("From", from);
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(url);
+        OutboundSMS record = new OutboundSMS();
+        Response response = null;
+        try {
+            response = CredentialHelperService.setCredential(target.request(), credential)
+                    .post(Entity.form(map), Response.class);
+            log.info("Response : {}", response);
+        } catch (Exception ex) {
+            log.error("error while hitting  twilio url :{}", ex.getMessage());
+            // throw new BusinessException("Something went wrong.Please try after sometime");
             result = "server_error";
             return;
         }
-
-        Message.Status status = message.getStatus();
-        if (Message.Status.ACCEPTED.equals(status)) {
-            OutboundSMS outboundSMS = new OutboundSMS();
-            outboundSMS.setCreationDate(Instant.now());
-            outboundSMS.setPurpose("OTP");
-            outboundSMS.setOtpCode(otp);
-            outboundSMS.setTo(to);
-            outboundSMS.setMessage(body);
-            outboundSMS.setResponse(result);
+        String value = response.readEntity(String.class);
+        JSONObject json = new JSONObject(value);
+        result = json.getString("status");
+        if ("accepted".equalsIgnoreCase(result)) {
+            log.info("Value : {}", value);
+            record.setCreationDate(Instant.now());
+            record.setPurpose("OTP");
+            record.setOtpCode(otp);
+            record.setTo(to);
+            record.setMessage(message);
+            record.setResponse(result);
             try {
-                crossStorageApi.createOrUpdate(defaultRepo, outboundSMS);
-            } catch (Exception e) {
-                LOG.error("error updating outboundSMS CEI: {}", e);
+                crossStorageApi.createOrUpdate(defaultRepo, record);
+            } catch (Exception ex) {
+                log.error("error updating twilio record :{}", ex.getMessage());
                 result = "server_error";
             }
         }
+    }
+
+    public void setTo(String to) {
+        this.to = to;
     }
 }
