c5ca0166adf492ba4f8e284b2abba40397619287
meveo.admin <no-mail@meveo.com>
2022-03-14 09:18:13 +0000

    Add JS script for Endpoint: verifyOtp

--- facets/javascript/endpoints/verifyOtp.js
+++ facets/javascript/endpoints/verifyOtp.js
@@ -1,60 +1,45 @@
-import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
-
-// the request schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this is used to validate and parse the request parameters
-const requestSchema = {
-  "title" : "verifyOtpRequest",
-  "id" : "verifyOtpRequest",
-  "default" : "Schema definition for verifyOtp",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object",
-  "properties" : {
-    "otp" : {
-      "title" : "otp",
-      "type" : "string",
-      "minLength" : 1
-    }
-  }
+const verifyOtp = async (parameters) =>  {
+	const baseUrl = window.location.origin;
+	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/verifyOtp/${parameters.to}`, baseUrl);
+	return fetch(url.toString(), {
+		method: 'POST', 
+		headers : new Headers({
+ 			'Content-Type': 'application/json'
+		}),
+		body: JSON.stringify({
+			otp : parameters.otp
+		})
+	});
 }
 
-// the response schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this could be used to parse the result
-const responseSchema = {
-  "title" : "verifyOtpResponse",
-  "id" : "verifyOtpResponse",
-  "default" : "Schema definition for verifyOtp",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object",
-  "properties" : {
-    "result" : {
-      "title" : "result",
-      "type" : "string",
-      "minLength" : 1
-    }
-  }
+const verifyOtpForm = (container) => {
+	const html = `<form id='verifyOtp-form'>
+		<div id='verifyOtp-to-form-field'>
+			<label for='to'>to</label>
+			<input type='text' id='verifyOtp-to-param' name='to'/>
+		</div>
+		<div id='verifyOtp-otp-form-field'>
+			<label for='otp'>otp</label>
+			<input type='text' id='verifyOtp-otp-param' name='otp'/>
+		</div>
+		<button type='button'>Test</button>
+	</form>`;
+
+	container.insertAdjacentHTML('beforeend', html)
+
+	const to = container.querySelector('#verifyOtp-to-param');
+	const otp = container.querySelector('#verifyOtp-otp-param');
+
+	container.querySelector('#verifyOtp-form button').onclick = () => {
+		const params = {
+			to : to.value !== "" ? to.value : undefined,
+			otp : otp.value !== "" ? otp.value : undefined
+		};
+
+		verifyOtp(params).then(r => r.text().then(
+				t => alert(t)
+			));
+	};
 }
 
-// should contain offline mock data, make sure it adheres to the response schema
-const mockResult = {};
-
-class verifyOtp extends EndpointInterface {
-	constructor() {
-		// name and http method, these are inserted when code is generated
-		super("verifyOtp", "POST");
-		this.requestSchema = requestSchema;
-		this.responseSchema = responseSchema;
-		this.mockResult = mockResult;
-	}
-
-	getRequestSchema() {
-		return this.requestSchema;
-	}
-
-	getResponseSchema() {
-		return this.responseSchema;
-	}
-}
-
-export default new verifyOtp();
+export { verifyOtp, verifyOtpForm };
